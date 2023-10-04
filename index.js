45fc48ab1a5cece66a6313c0dc20683c3a98d6d2
Ilan Rakhmanov <info@chaingpt.org>
2023-10-03 22:45:46 -0400

    Update index.js

--- index.js
+++ index.js
@@ -134,6 +134,145 @@ const contractAddresses = [
   },
 ];
 
+
+// List of contract addresses with additional information
+const contractAddressesCMC = [
+  {
+    address: '0x5930976fc5eacccea648555cc12438a3278a9fbe',
+    chain: 'BSC',
+    type: 'Gnosis Multi-Sig Wallet (Team-Controlled',
+    wallet: 'Team Controlled Wallet - Multisig', 
+  },
+  
+  {
+    address: '0x699cfbbb29f7734ccb1a342b3dfbeac391423d48',
+    chain: 'BSC',
+    type: 'Gnosis Multi-Sig Wallet (Team-Controlled',
+    wallet: 'Team Controlled Wallet - Multisig', 
+  },
+  
+  {
+    address: '0xbc9755809702906a9351c846e33d840541f19d73',
+    chain: 'BSC',
+    type: 'Gnosis Multi-Sig Wallet (Team-Controlled',
+    wallet: 'Team Controlled Wallet - Multisig', 
+  },
+  {
+    address: '0x1f7bAAf93e0449394e80f3A24c14fB2fA667495c',
+    chain: 'BSC',
+    type: 'TeamFinance Vesting',
+    wallet: 'KOLs Round (ref: tokenomics)', 
+  },
+  {
+    address: '0x77A8b449e7cd61dd54B755034Baf9d00EeeD1076',
+    chain: 'BSC',
+    type: 'TeamFinance Vesting',
+    wallet: 'Seedify IDO Fee (3.5%) in tokens', 
+  },
+  {
+    address: '0x0aaf30015ee2393dbeab2d8830f6d244f2dfa0f1',
+    chain: 'BSC',
+    type: 'TeamFinance Vesting',
+    wallet: 'Farming/Staking (ref: tokenomics)', 
+  },
+  {
+    address: '0xb63ea5700834975e8349cfd2d54216f7749b0e49',
+    chain: 'BSC',
+    type: 'TeamFinance Vesting',
+    wallet: 'Liquidity Allocation (ref: tokenomics)', 
+  },
+  {
+    address: '0x9d9ceebf7fb22df325c37591c43c7c0e0f36c6a2',
+    chain: 'BSC',
+    type: 'TeamFinance Vesting',
+    wallet: 'Private B Round (ref: tokenomics)', 
+  },
+  {
+    address: '0x2d842cf79aa3d6bcef9e37dd9d8809ebab813ad5',
+    chain: 'BSC',
+    type: 'TeamFinance Vesting',
+    wallet: 'Private A Round (ref: tokenomics)', 
+  },
+  {
+    address: '0x73e49c140c35fc1cc0c716f00d435e22ab8c1305',
+    chain: 'BSC',
+    type: 'TeamFinance Vesting',
+    wallet: 'Treasury, Team, DAO Fund, Marketing, Development (ref: tokenomics)', 
+  },
+  {
+    address: '0x39f119f1d89ba0e14daded2a5901f3c42135726c',
+    chain: 'BSC',
+    type: 'TeamFinance Vesting',
+    wallet: 'Available Advisory Tokens (ref: tokenomics)', 
+  },
+  {
+    address: '0x915a6fF38cab0bB0B027179D7b5f196DAB25C626',
+    chain: 'BSC',
+    type: 'IDO Round Vesting',
+    wallet: 'Seedify IDO', 
+  },
+  {
+    address: '0x6dAa1b5F9cF268AF2fd93Dff8083E2d3ca237008',
+    chain: 'BSC',
+    type: 'Crowdfunding Vesting',
+    wallet: 'YAYNetwork', 
+  },
+  {
+    address: '0x98E9EBc9539d674cbcd505b6A6483991d50c2356',
+    chain: 'BSC',
+    type: 'Crowdfunding Vesting',
+    wallet: 'Decubate', 
+  },
+  {
+    address: '0xd377766831DE29B39Ea45687983F9F590add517f',
+    chain: 'BSC',
+    type: 'IDO Round Vesting',
+    wallet: 'EnjinStarter', 
+  },
+  {
+    address: '0x436CE2ce8d8d2Ccc062f6e92faF410DB4d397905',
+    chain: 'BSC',
+    type: 'IDO Round Vesting',
+    wallet: 'Poolz', 
+  },
+      {
+    address: '0x926D28476A4bB4F7E4c27C913a3495b3d7393006',
+    chain: 'BSC',
+    type: 'TeamFinance  Contract (Locked)',
+    wallet: 'Seedify  Incubation Fee (1%)', 
+  },
+  {
+    address: '0xd58DE7168b7d22048db5EdAafa07265B70407Bc0',
+    chain: 'BSC',
+    type: 'IDO Round Vesting',
+    wallet: 'WePad', 
+  },
+  {
+    address: '0x765a6ee976137801F2661c3644E1fde369A8ED18',
+    chain: 'BSC',
+    type: 'Staking Pools',
+    wallet: 'CGPT Staking Rewards & Staked (via Decubate)', 
+  },
+  {
+    address: '0x62A402DEf6Ca37E9CA7a544bE34954748088CCEE',
+    chain: 'BSC',
+    type: 'Staking Pools',
+    wallet: 'ChainGPT Pad Staking Pool', 
+  },
+  {
+    address: '0x28FBFA75850E246BdD454A0e76FeAA42D771757B',
+    chain: 'BSC',
+    type: 'Staking Pools',
+    wallet: 'ChainGPT Pad Staking Pool - Rewards Distributor', 
+  },
+  {
+    address: '0x3A53269cAE3281155e51d06Ffe14fdE8DC3662f7',
+    chain: 'BSC',
+    type: 'Claim Portal Balance',
+    wallet: 'Claim Portal Balance (KOLs/Advisors/Private)', 
+  },
+];
+
 const contractAddresses2 = [
   {
     address: '0x1f3215f8E5b449BbD30824037c6b55f61c4E9D19',
@@ -760,6 +899,55 @@ app.get('/supply', async (req, res) => {
     const balances = [];
 
     for (const { address, chain, type, wallet, name } of contractAddresses) {
+      const url = `https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=${cgptContractAddress}&address=${address}&tag=latest&apikey=${apiKey}`;
+      const response = await axios.get(url);
+      const balance = parseInt(response.data.result);
+
+      balances.push({ address, balance, chain, type, wallet, name });
+    }
+
+    balances.sort((a, b) => b.balance - a.balance); // Sort balances in descending order
+
+    let totalBalance = 0;
+    let tableRows = '';
+
+    for (const { address, balance, chain, type, wallet } of balances) {
+      totalBalance += balance;
+      tableRows += `<tr>
+        <td>${address}</td>
+        <td>${Math.floor(balance / 10 ** 18).toLocaleString()}</td>
+        <td>${chain}</td>
+        <td>${type}</td>
+        <td>${wallet}</td>
+      </tr>`;
+    }
+
+    const totalSupplyEndpointResult = await getTotalSupply();
+    const burntTokens = 1000000000 - Math.floor(totalSupplyEndpointResult / 10 ** 18);
+    const totalSupply = 1000000000 - Math.floor(totalBalance / 10 ** 18) - burntTokens;
+
+    const htmlResponse = `${totalSupply.toLocaleString()}`;
+
+    cache.set('supply', htmlResponse); // Cache the supply response
+
+    res.send(htmlResponse);
+  } catch (error) {
+    res.status(500).send('Error fetching data');
+  }
+});
+
+
+app.get('/api', async (req, res) => {
+  const cachedSupply = cache.get('supply');
+  if (cachedSupply !== undefined) {
+    res.send(cachedSupply);
+    return;
+  }
+
+  try {
+    const balances = [];
+
+    for (const { address, chain, type, wallet, name } of contractAddressesCMC) {
       const url = `https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=${cgptContractAddress}&address=${address}&tag=latest&apikey=${apiKey}`;
       const response = await axios.get(url);
       const balance = parseInt(response.data.result);
