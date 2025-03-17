0cc1cec88abe0765cfa96d2fa58f21c9b682adc4
Ilan Rakhmanov <26630467+ceoguy@users.noreply.github.com>
2025-03-16 17:46:08 -0700

    Update index.js

--- index.js
+++ index.js
@@ -74,6 +74,12 @@ const contractAddresses = [
     type: 'TeamFinance  Contract (Locked)',
     wallet: 'Seedify  Incubation Fee (1%)', 
   },
+      {
+    address: '0x1f3215f8E5b449BbD30824037c6b55f61c4E9D19',
+    chain: 'BSC',
+    type: 'Treasury Allocation',
+    wallet: 'Seedify  Advisory Fee (1%)', 
+  },
 ];
 
 
